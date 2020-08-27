966e9bc012d1bed10d63593132a2e3597b7d3e3e
Hongxu <hongxuchen1989@gmail.com>
2020-08-27 11:41:20 +0800

    fix cli

--- src/main/scala/sg/edu/ntu/console/FeatureSerializer.scala
+++ src/main/scala/sg/edu/ntu/console/FeatureSerializer.scala
@@ -1,9 +1,13 @@
 package sg.edu.ntu.console
 
-import io.shiftleft.fuzzyc2cpg.FuzzyC2Cpg
+import java.nio.file.{Files, Path}
+
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.fuzzyc2cpg.{FuzzyC2Cpg, SourceFiles}
 import org.slf4j.LoggerFactory
 import sg.edu.ntu.ProjectMD
 
+import scala.collection.mutable.ListBuffer
 import scala.util.control.NonFatal
 
 
@@ -15,11 +19,12 @@ object FeatureSerializer extends App {
 
   case class ParserConfig(inputPaths: Set[String] = Set.empty,
                           projectMD: ProjectMD = ProjectMD.DUMMY_PROJ,
-                          semanticsFile: String = CpgLoader.defaultSemanticsFile,
-                          sourceFileExtensions: Set[String] = defaultExts,
-                          preprocessorConfig: PreprocessorConfig = PreprocessorConfig())
+                          srcExts: Set[String] = defaultExts,
+                          forceUpdateCPG: Boolean = false,
+                          forceUpdateSM: Boolean = false,
+                          ppConfig: PreprocessorConfig = PreprocessorConfig())
 
-  case class PreprocessorConfig(preprocessorExecutable: String = DEFAULT_FUZZYPPCLI_PATH,
+  case class PreprocessorConfig(preprocessorExecutable: String = DEFAULT_FUZZYPPCLI,
                                 verbose: Boolean = true,
                                 includeFiles: Set[String] = Set.empty,
                                 includePaths: Set[String] = Set.empty,
@@ -29,7 +34,57 @@ object FeatureSerializer extends App {
       includeFiles.nonEmpty || includePaths.nonEmpty || defines.nonEmpty || undefines.nonEmpty
   }
 
-  def generateCpg(config: ParserConfig): Unit = {
+  def runPP(fuzzyC2Cpg: FuzzyC2Cpg,
+            sourcePaths: Set[String],
+            srcExts: Set[String],
+            includeFiles: Set[String],
+            includePaths: Set[String],
+            defs: Set[String],
+            undefs: Set[String],
+            ppExec: String): (Int, Path) = {
+    // Create temp dir to store preprocessed source.
+    val ppPath = Files.createTempDirectory("fuzzyc2cpg_pp_")
+    logger.info(s"Writing preprocessed files to [$ppPath]")
+
+    val ppLogFile = Files.createTempFile("fuzzyc2cpg_pp_log", ".txt").toFile
+    logger.info(s"Writing preprocessor logs to [$ppLogFile]")
+
+    val sourceFileNames = SourceFiles.determine(sourcePaths, srcExts)
+
+    val commandBuffer = new ListBuffer[String]()
+    commandBuffer.appendAll(List(ppExec, "--verbose", "-o", ppPath.toString))
+    if (sourceFileNames.nonEmpty) commandBuffer.appendAll(List("-f", sourceFileNames.mkString(",")))
+    if (includeFiles.nonEmpty) commandBuffer.appendAll(List("--include", includeFiles.mkString(",")))
+    if (includePaths.nonEmpty) commandBuffer.appendAll(List("-I", includePaths.mkString(",")))
+    if (defs.nonEmpty) commandBuffer.appendAll(List("-D", defs.mkString(",")))
+    if (undefs.nonEmpty) commandBuffer.appendAll(List("-U", defs.mkString(",")))
+
+    val cmd = commandBuffer.toList
+
+    // Run preprocessor
+    logger.info("Running preprocessor...")
+    val process = new ProcessBuilder()
+      .redirectOutput(ppLogFile)
+      .redirectError(ppLogFile)
+      .command(cmd: _*)
+      .start()
+    val exitCode = process.waitFor()
+
+    if (exitCode == 0) {
+      logger.info(s"Preprocessing complete, files written to [$ppPath], starting CPG generation...")
+    } else {
+      logger.error(
+        s"Error occurred whilst running preprocessor. Log written to [$ppLogFile]. rc=[$exitCode]; analyzing w/o pp")
+    }
+    (exitCode, ppPath)
+  }
+
+  /**
+    *
+    * @param config parse config
+    * @return an active `Cpg` to be used (need closing before exiting)
+    */
+  def generateCpg(config: ParserConfig): Cpg = {
 
     import better.files.Dsl._
     val dbDir = pwd / DB_DIR
@@ -38,24 +93,45 @@ object FeatureSerializer extends App {
       mkdir(dbDir)
     }
     val rawDB = dbDir / config.projectMD.asCpgFileName
+    val rawDBFileName = rawDB.toString()
+
+    if (rawDB.exists) {
+      if (!config.forceUpdateCPG) {
+        logger.info(s"${rawDB} exists, do nothing")
+        val cpg = CpgLoader.loadFromOdb(rawDBFileName)
+        return cpg
+      } else {
+        logger.info(s"${rawDB} force updating")
+      }
+    }
+
     val rawDBFilePath = rawDB.toString()
-    logger.info(s"output raw cpg to ${rawDB}")
+    logger.info(s"output raw cpg to ${rawDBFilePath}")
 
     val fuzzyc = new FuzzyC2Cpg()
-    if (config.preprocessorConfig.usePreprocessor) {
+    if (config.ppConfig.usePreprocessor) {
       logger.info("running w/ fuzzyppcli")
-      fuzzyc.runWithPreprocessorAndOutput(
+      val (exitCode, ppPath) = runPP(
+        fuzzyc,
         config.inputPaths,
-        config.sourceFileExtensions,
-        config.preprocessorConfig.includeFiles,
-        config.preprocessorConfig.includePaths,
-        config.preprocessorConfig.defines,
-        config.preprocessorConfig.undefines,
-        config.preprocessorConfig.preprocessorExecutable
+        config.srcExts,
+        config.ppConfig.includeFiles,
+        config.ppConfig.includePaths,
+        config.ppConfig.defines,
+        config.ppConfig.undefines,
+        config.ppConfig.preprocessorExecutable,
       )
+      if (exitCode == 0) {
+        val cpg = fuzzyc.runAndOutput(Set(ppPath), config.srcExts, None)
+        cpg
+      } else {
+        val cpg = fuzzyc.runAndOutput(config.inputPaths, config.srcExts, Some(rawDBFilePath))
+        cpg
+      }
     } else {
       logger.info("running w/o fuzzyppcli")
-      fuzzyc.runAndOutput(config.inputPaths, config.sourceFileExtensions, Some(rawDBFilePath)).close()
+      val cpg = fuzzyc.runAndOutput(config.inputPaths, config.srcExts, Some(rawDBFilePath))
+      cpg
     }
 
   }
@@ -71,38 +147,35 @@ object FeatureSerializer extends App {
         .text("project metadata to be specified")
         .required()
         .action((x, c) => c.copy(projectMD = ProjectMD(x)))
-      opt[String]("semfile")
-        .text("data flow semantics file")
-        .action((x, c) => c.copy(semanticsFile = x))
       opt[String]("src-ext")
         .unbounded()
         .text("source file extensions to include when gathering source files.")
-        .action((pat, cfg) => cfg.copy(sourceFileExtensions = cfg.sourceFileExtensions + pat))
+        .action((pat, cfg) => cfg.copy(srcExts = cfg.srcExts + pat))
       opt[String]("include")
         .unbounded()
         .text("header include files")
         .action((incl, cfg) =>
-          cfg.copy(preprocessorConfig =
-            cfg.preprocessorConfig.copy(includeFiles = cfg.preprocessorConfig.includeFiles + incl)))
+          cfg.copy(ppConfig =
+            cfg.ppConfig.copy(includeFiles = cfg.ppConfig.includeFiles + incl)))
       opt[String]('I', "")
         .unbounded()
         .text("header include paths")
         .action((incl, cfg) =>
-          cfg.copy(preprocessorConfig =
-            cfg.preprocessorConfig.copy(includePaths = cfg.preprocessorConfig.includePaths + incl)))
+          cfg.copy(ppConfig =
+            cfg.ppConfig.copy(includePaths = cfg.ppConfig.includePaths + incl)))
       opt[String]('D', "define")
         .unbounded()
         .text("define a MACRO value")
         .action((d, cfg) =>
-          cfg.copy(preprocessorConfig = cfg.preprocessorConfig.copy(defines = cfg.preprocessorConfig.defines + d)))
+          cfg.copy(ppConfig = cfg.ppConfig.copy(defines = cfg.ppConfig.defines + d)))
       opt[String]('U', "undefine")
         .unbounded()
         .text("undefine a MACRO value")
         .action((u, cfg) =>
-          cfg.copy(preprocessorConfig = cfg.preprocessorConfig.copy(defines = cfg.preprocessorConfig.undefines + u)))
+          cfg.copy(ppConfig = cfg.ppConfig.copy(defines = cfg.ppConfig.undefines + u)))
       opt[String]("pp-exe")
         .text("path to the preprocessor executable")
-        .action((s, cfg) => cfg.copy(preprocessorConfig = cfg.preprocessorConfig.copy(preprocessorExecutable = s)))
+        .action((s, cfg) => cfg.copy(ppConfig = cfg.ppConfig.copy(preprocessorExecutable = s)))
       help("help").text("display this help message")
     }.parse(args, ParserConfig())
 
--- src/main/scala/sg/edu/ntu/console/package.scala
+++ src/main/scala/sg/edu/ntu/console/package.scala
@@ -2,9 +2,9 @@ package sg.edu.ntu
 
 package object console {
 
-  val DEFAULT_CPG_OUT_FILE:String = "cpg" + RawSuffix
+  val DEFAULT_CPG_OUT_FILE: String = "cpg" + RawSuffix
   val DEFAULT_CPG_IN_FILE: String = DEFAULT_CPG_OUT_FILE
-  val DEFAULT_FUZZYPPCLI_PATH:String = "/usr/bin/fuzzyppcli"
+  val DEFAULT_FUZZYPPCLI: String = "/usr/bin/fuzzyppcli"
 
   val DB_DIR = "smdb"
 
